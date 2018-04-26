21b3120ec3c4637040cff2517d92ff48eee76df0
LiuLiFei <1061334689@qq.com>
2018-04-26 18:30:07 +0800

    对象 转Json 格式

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,8 +1,13 @@
+import com.google.gson.Gson;
 import entity.BookEntity;
 import factory.GetSqlSessionFactory;
 
 import mapper.BookMapper;
 import org.apache.ibatis.session.SqlSession;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class main {
 
@@ -11,15 +16,30 @@ public class main {
         //如果sqlsessionFactory.openSession(true)参数是布尔值，如果设置为true，就不需要commit提交事务了
         SqlSession session = GetSqlSessionFactory.getSqlSessionFactory().openSession();
         try {
+            Gson gson = new Gson();
             BookMapper bookMapper = session.getMapper(BookMapper.class);
             //BookEntity bookEntity = bookMapper.selectBook(5);
             BookEntity bookEntity = new BookEntity();
             //bookEntity.setId(-1); insert 插入成功会返还最新的主键;
-            bookEntity.setName("LiuLiFei");
-            bookMapper.insertBook(bookEntity);
 
+            bookEntity.setId(1);
+            bookEntity.setName("我是更新来的值");
+
+            List<BookEntity> bookEntityList = bookMapper.getSelectBook("测试多行更新");
+            System.out.println(bookEntityList);
+            System.out.println(gson.toJson(bookEntityList));
+            //bookMapper.insertBook(bookEntity);
+            //bookMapper.selectBook(1);
+            //int row = bookMapper.updateBook(bookEntity); //更新的额时候会返回一个改变的行数
+
+            //Map<String,String> map = new HashMap<String, String>();
+            //map.put("sourceName","LiuLiFei");
+            //map.put("newName","测试多行更新");
+            //int row = bookMapper.updateBookName(map);
+            //System.out.println("更新的行数："+row);
             //切记：增删改操作时，要执行commit操作 查询不需要执行commit();
-            session.commit();
+            //session.commit();
+
             System.out.println(bookEntity);
         } finally {
             session.close();
--- src/main/java/mapper/BookMapper.java
+++ src/main/java/mapper/BookMapper.java
@@ -2,9 +2,14 @@ package mapper;
 
 import entity.BookEntity;
 
+import java.util.List;
+import java.util.Map;
 
 
 public interface BookMapper {
     BookEntity selectBook(int id);
+    List<BookEntity> getSelectBook(String name);
     void insertBook(BookEntity bookEntity);
+    int updateBook(BookEntity bookEntity);
+    int updateBookName(Map<String,String> map);
 }
